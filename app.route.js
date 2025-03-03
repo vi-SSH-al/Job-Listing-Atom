15c5fecb513bbd7e165058d191cac2318f9b08ae
vaidik24 <vaidik24pandya@gmail.com>
2025-03-03 18:38:24 +0530

    demo set up is done and running

--- app.route.js
+++ app.route.js
@@ -1,7 +1,7 @@
 "use strict";
 var app = angular.module("job_listing", ["ngRoute"]);
 app.config(function ($routeProvider) {
-  $routeProvider.when("/demo", {
+  $routeProvider.when("/", {
     templateUrl: "./Components/Demo/demo.html",
     controller: "demoController",
   });
