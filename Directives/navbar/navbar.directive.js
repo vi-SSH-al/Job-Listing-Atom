efb7c2a3d965088d06a5ec3098ee8136a77eb20d
Mr-Bhagatwala <154253285+Mr-Bhagatwala@users.noreply.github.com>
2025-03-04 12:43:55 +0530

    navbar responsive add

--- Directives/navbar/navbar.directive.js
+++ Directives/navbar/navbar.directive.js
@@ -1,7 +1,31 @@
 "use strict";
-angular.module("job_listing").directive("navbar", function () {
+angular.module("job_listing").directive("navbar", function ($document) {
   return {
     restrict: "E",
     templateUrl: "./Directives/navbar/navbar.html",
+    controller: function ($scope) {
+      $scope.menuOpen = false;
+
+      // Logo Image URL
+      $scope.logoUrl =
+        "https://media.licdn.com/dms/image/v2/D5622AQE9zq12SHD6sA/feedshare-shrink_800/B56ZUY4KjhHQAg-/0/1739879136122?e=2147483647&v=beta&t=3gNRg4sDzYd7XY2dgYOkZ2VrnKTrTmkQn1QdrxHnD10";
+
+      // Toggle Mobile Menu
+      $scope.toggleMenu = function () {
+        $scope.menuOpen = !$scope.menuOpen;
+      };
+
+      // Close menu when clicking outside
+      $document.on("click", function (event) {
+        var isClickedInside = angular
+          .element(event.target)
+          .closest(".navbar, .dropDownMenuForNavbar").length;
+        if (!isClickedInside) {
+          $scope.$apply(function () {
+            $scope.menuOpen = false;
+          });
+        }
+      });
+    },
   };
 });
